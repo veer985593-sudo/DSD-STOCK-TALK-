65d86895b6924a7c2b33d3b8a36d4dfe5ec86f8f
DSD STOCK TALK™ <veer985593@gmail.com>
2026-05-27 11:57:46 +0530

    Fixed indentation and spacing

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from datetime import datetime
 import pandas as pd
 import plotly.graph_objects as go
 import yfinance as yf
-from streamlit_autorefresh import st_autorefresh
 
 # Must be first Streamlit command
 st.set_page_config(
